a2c39ff850569fb1fc7d7a2bfaff4c45f8611e49
Ashlynn Mesidor <ashlynnmesidor@yahoo.com>
2019-03-20 16:52:41 -0400

    Feat(routing/navbar): add icons

--- App.js
+++ App.js
@@ -1,77 +1,40 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Image } from "react-native"
 import { NativeRouter, Route, Link } from "react-router-native"
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
-const Home = () => <Text style={styles.header}>Home</Text>;
+const Home = () => <Text>Home</Text>
 
-const About = () => <Text style={styles.header}>About</Text>;
+const Scan = () => <Text>Scan</Text>
 
-const Topic = ({ match }) => (
-  <Text style={styles.topic}>{match.params.topicId}</Text>
-);
+const Settings = () => <Text>Settings</Text>
 
-const Topics = ({ match }) => (
-    <View>
-      <Text style={styles.header}>Topics</Text>
-      <View>
-        <Link
-          to={`${match.url}/rendering`}
-          style={styles.subNavItem}
-          underlayColor="#f0f4f7"
-        >
-          <Text>Rendering with React</Text>
-        </Link>
-        <Link
-          to={`${match.url}/components`}
-          style={styles.subNavItem}
-          underlayColor="#f0f4f7"
-        >
-          <Text>Components</Text>
-        </Link>
-        <Link
-          to={`${match.url}/props-v-state`}
-          style={styles.subNavItem}
-          underlayColor="#f0f4f7"
-        >
-          <Text>Props v. State</Text>
-        </Link>
-      </View>
-  
-      <Route path={`${match.path}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <Text style={styles.topic}>Please select a topic.</Text>}
-      />
-    </View>
-  );
-  
   export default App = () => (
     <NativeRouter>
       <View style={styles.container}>
         <View style={styles.nav}>
           <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
-            <Text>Home</Text>
+            <Icon name="home-outline" size={50}/>
           </Link>
-          <Link to="/about" underlayColor="#f0f4f7" style={styles.navItem}>
-            <Text>About</Text>
+          <Link to="/scan" underlayColor="#f0f4f7" style={styles.navItem}>
+            <Icon name="qrcode-scan" size={50}/>
           </Link>
-          <Link to="/topics" underlayColor="#f0f4f7" style={styles.navItem}>
-            <Text>Topics</Text>
+          <Link to="/settings" underlayColor="#f0f4f7" style={styles.navItem}>
+            <Icon name="settings-outline" size={50}/>
           </Link>
         </View>
   
         <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
+        <Route path="/scan" component={Scan} />
+        <Route path="/settings" component={Settings} />
       </View>
     </NativeRouter>
   );
 
   const styles = StyleSheet.create({
     container: {
-      marginTop: 25,
-      padding: 10
+      bottom: 0,
+      position: "absolute"
     },
     header: {
       fontSize: 20
